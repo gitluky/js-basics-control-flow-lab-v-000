07e62a82f2c0a1b0723af11be8f0b6796566dd06
Jonathan Luk <jluk87@gmail.com>
2019-07-29 21:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function switchOnCharmFromTip(tip) {
 
   switch (tip) {
     case 'generous':
-      response = 'Thank you so much';
+      response = 'Thank you so much.';
       break;
     case 'not as generous':
       response = 'Thank you.'
