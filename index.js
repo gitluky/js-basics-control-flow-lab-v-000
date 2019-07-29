dea7595faf55372d79def21b449f2fb416d6dcf3
Jonathan Luk <jluk87@gmail.com>
2019-07-29 20:58:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let response;
+
+  if (distance > 2500) {
+    response = 'No can do.';
+  } else if ( distance > 2000 ) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (distance < 400 ) {
+    response = 'This one is on me!';
+  }
+
+  return response;
+
+}
